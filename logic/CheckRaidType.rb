3ff320ecc4ad932555fb5ffc09ac3a47494e9e22
Siv <siv.lim777@gmail.com>
2016-10-11 12:48:01 -0700

    added more logic to CheckRaidType.rb

--- logic/CheckRaidType.rb
+++ logic/CheckRaidType.rb
@@ -10,13 +10,31 @@
 class CheckRaidType
     def initialize()
     @@raidTypeArray = Array.new
+    @@cleanRaidTypeArray = Array.new
     end
+
+
+#get raid type
 
     def getRaidType
+    #tries hpacucli command and cleans the output of leading/trailing whitelines
+    #and empty elements
     @@raidTypeArray = `sudo hpacucli ctrl slot=0 pd all show status`
+    @@raidTypeArray.each do |i|
+    @@cleanRaidTypeArray.push(i.strip)
+    @@cleanRaidTypeArray.delete_if { |x| x.empty? }
     end
+    #check if raid type is HP (right now it only checks for HP later
+    #checks for different raid type will be added in the future)
+    puts @@cleanRaidTypeArray.length
+    end
+
+
 end
+
+
+
+
 #sudo: hpacucli: command not found
 test = CheckRaidType.new
-
-puts test.getRaidType
+test.getRaidType
