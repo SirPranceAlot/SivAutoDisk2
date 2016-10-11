a7a30347ac67ed9537a7466cb975d8aba65cfeea
Siv <siv.lim777@gmail.com>
2016-10-11 16:46:06 -0700

    SivAutoDisk.rb controller is now able to import classes from the logic folder and display the menu from the DriveReplacementMenu class

--- SivAutoDisk2.rb
+++ SivAutoDisk2.rb
@@ -1,4 +1,19 @@
 #!/usr/bin/ruby
 #
-##
+#SivAutoDisk2 main controller
 
+#Gets the current directory to be added to $LOAD_PATH
+#need this in order to import classes from the logic folder
+currentDirectory = `pwd`
+
+#Adds the logic directory to $LOAD_PATH
+$LOAD_PATH << File.dirname(currentDirectory.strip + "/logic")
+puts $LOAD_PATH
+
+#importing classes
+require "DriveReplacementMenu"
+
+#initializing object
+menu = DriveReplacementMenu.new
+#display menu
+menu.displayMenu
--- logic/DriveReplacementMenu.rb
+++ logic/DriveReplacementMenu.rb
@@ -1,19 +1,23 @@
 #!usr/bin/ruby
 #
-#declaring required classes
+#importing required classes
 require "CheckRaidType"
 require "InfoGatherer"
 
 class DriveReplacementMenu
-#initializing classes to variables
-checkRaidType = CheckRaidType.new
-info  = InfoGatherer.new
 
-puts "~~SivAutoDisk 2~~\n\n"
-puts "This script is for automating disk replacements."
-puts "Hostname: " + info.getHostName
-puts "Disk(s) detected requiring repair:"
-puts "System type:"
-puts "Raid type: " + checkRaidType.getRaidType
+    #display menu method
+    def displayMenu
+    #initializing objects
+    checkRaidType = CheckRaidType.new
+    info  = InfoGatherer.new
+
+    puts "~~SivAutoDisk 2~~\n\n"
+    puts "This script is for automating disk replacements."
+    puts "Hostname: " + info.getHostName
+    puts "Disk(s) detected requiring repair:"
+    puts "System type:"
+    puts "Raid type: " + checkRaidType.getRaidType
+    end
 end
 
