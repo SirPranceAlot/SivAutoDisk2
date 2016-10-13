50693e436f29935abc2d8b6d671a396d78195266
Siv <siv.lim777@gmail.com>
2016-10-13 14:55:50 -0700

    added logic to detect and store failed drive information

--- logic/HpacucliModule.rb
+++ logic/HpacucliModule.rb
@@ -9,18 +9,34 @@ class HpacucliModule < Module
     def initialize
         @failedPhysicalDrives = Array.new
 	@failedLogicalDrives = Array.new
+	@failedHpDriveNames = Array.new
+	@failedHpDriveAndStatus = Hash.new
 	@unmountedDrives = Array.new
     end
 
+#get a lits of failed drives, cleans the list up a little, gets the drive number, the hp drive number format, and the status message for the drive
     def checkFailedPhysicalDrives
 	@failedPhysicalDrives = `sudo hpacucli ctrl slot=0 pd all show status`
 	@cleanFailedPhysicalDrives = Array.new
 	@failedPhysicalDrives.each do |e|
             @cleanFailedPhysicalDrives.push(e.chomp)
-         end
+        end
+
 	@cleanFailedPhysicalDrives.reject! {|e| e.empty?}
-	puts @cleanFailedPhysicalDrives
-	#\w+ (\S+) \(port 1I:box 1:bay (\d), \S* GB\): (\w+)
+
+
+	#get the failed drive number and status message and store in @failedHpDriveAndStatus
+	#get hp failed drive number format and store in @failedHpDriveNames
+	@cleanFailedPhysicalDrives.each do |d|
+	    if d =~ /\w+ (\S+) \(port 1I:box 1:bay (\d), \S* GB\): (\w+)/
+		if $3 == "Failed" || $3 == "Predictive Failure"
+		   @failedHpDriveAndStatus[:"#{$2}"] = $3
+		   @failedHpDriveNames.push($1)
+		end
+	    end
+        end
+	puts @failedHpDriveAndStatus
+	puts @failedHpDriveNames	
     end
 
 
